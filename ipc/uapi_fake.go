6cd5922a04de9370d48cf742751d4cdcc6ce65e1
Brad Fitzpatrick <bradfitz@tailscale.com>
2023-08-24 14:39:23 -0700

    all: adjust build tags for plan9
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- ipc/uapi_fake.go
+++ ipc/uapi_fake.go
@@ -1,3 +1,5 @@
+//go:build wasm || plan9
+
 /* SPDX-License-Identifier: MIT
  *
  * Copyright (C) 2017-2023 WireGuard LLC. All Rights Reserved.
@@ -5,7 +7,7 @@
 
 package ipc
 
-// Made up sentinel error codes for {js,wasip1}/wasm.
+// Made up sentinel error codes for {js,wasip1}/wasm, and plan9.
 const (
 	IpcErrorIO        = 1
 	IpcErrorInvalid   = 2
