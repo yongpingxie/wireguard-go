124bae60edc8274a849c6bd4e3f5091c0224f7d4
GL.iNet-Yongping.Xie <yongping.xie@gl-inet.com>
2024-07-14 19:30:38 -0700

    fix: fix infinite memory bug
    Fix memory leak bug in openwrt environment.
    
    Signed-off-by: GL.iNet-Yongping.Xie <yongping.xie@gl-inet.com>

--- device/queueconstants_default.go
+++ device/queueconstants_default.go
@@ -10,10 +10,11 @@ package device
 import "github.com/tailscale/wireguard-go/conn"
 
 const (
-	QueueStagedSize            = conn.IdealBatchSize
-	QueueOutboundSize          = 1024
-	QueueInboundSize           = 1024
-	QueueHandshakeSize         = 1024
-	MaxSegmentSize             = (1 << 16) - 1 // largest possible UDP datagram
-	PreallocatedBuffersPerPool = 0             // Disable and allow for infinite memory growth
+	QueueStagedSize    = conn.IdealBatchSize
+	QueueOutboundSize  = 1024
+	QueueInboundSize   = 1024
+	QueueHandshakeSize = 1024
+	MaxSegmentSize     = (1 << 16) - 1 // largest possible UDP datagram
+	//PreallocatedBuffersPerPool = 0             // Disable and allow for infinite memory growth
+	PreallocatedBuffersPerPool = 1024
 )
